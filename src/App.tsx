03c95fc8cf2d92cc745bde2c4a6a583d09337cb9
wildpow <element1028@gmail.com>
2019-10-19 17:37:27 -0700

    Switch to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Disc Golf App</h1>
+        <p>With TypeScript!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;

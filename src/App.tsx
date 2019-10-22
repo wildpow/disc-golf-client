4f966f06a6fd4a1b0157c1e6d4db2f4c10abf6f5
wildpow <element1028@gmail.com>
2019-10-22 15:13:30 -0700

    New Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import Home from "./Containers/Home";
+import Profile from "./Containers/Profile";
+import NewGame from "./Containers/NewGame";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Disc Golf App</h1>
-        <p>With TypeScript!</p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Disc Golf App</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/newGame">New Game</Link>
+          </nav>
+        </header>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/newGame" component={NewGame} />
+      </div>
+    </Router>
   );
 };
 
---
+++ src/Containers/NewGame.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewGame: React.FC = () => {
+  return (
+    <div>
+      <h1>NewGame</h1>
+    </div>
+  );
+};
+
+export default NewGame;
